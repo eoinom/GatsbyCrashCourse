2b2bbf290606ab5f19090e95286073bcb345a2e0
Eoin O'Malley <eomalley@letsgetchecked.com>
2021-02-15 18:20:09 +0000

    WIP

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react"
+import Link from "gatsby-link"
 
-export default function Template ({data}) {
+export default function Template({ data }) {
   const post = data.markdownRemark
 
   return (
     <div>
-      <Link to ="/blog">Go Back</Link>
+      <Link to="/blog">Go Back</Link>
       <hr />
       <h1>{post.frontmatter.title}</h1>
-      <h4>Posts by {post.frontmatter.author} on {post.frontmatter.date}</h4>
-      <div dangerouslySetInnerHTML={{ __html: post.html}} />
+      <h4>
+        Posts by {post.frontmatter.author} on {post.frontmatter.date}
+      </h4>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
   )
 }
 
-export const postQuery = graphql `
-query BlogPostByPath($path: String!) {
-  markdownRemark(frontmatter: {path: {eg: $path } }){
-    htmlfrontmatter {
-      path
-      title
-      author 
-      date
+export const postQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+        author
+        date
+      }
     }
   }
-}`
+`
